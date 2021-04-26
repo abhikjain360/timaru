2319118061981a3013be5332a0b6fd59459e83db
Abhik Jain <abhikjain360@gmail.com>
2021-04-26 19:21:06 +0530

    xtasks ci command addede

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,27 @@
+use xshell::{cwd, cmd, pushd};
+
+mod flags;
+use flags::{XtaskCmd, Xtask};
+
+fn ci() -> xshell::Result<()> {
+    {
+        let _p = pushd("timaru")?;
+        cmd!("cargo fmt").run()?;
+        cmd!("cargo clippy").run()?;
+        cmd!("cargo build").run()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let flags = Xtask::from_env().expect("unable to parse flags");
+
+    match flags.subcommand {
+        XtaskCmd::Ci(_) => {
+            // TODO: write CI tests here
+            ci().unwrap();
+        }
+        XtaskCmd::Help(_) => {
+            println!("{}", Xtask::HELP);
+        }
+    }
 }
